43b62af2f05580cb6aca12e6de6321cb40054ff5
ram <ramkrishan@kontikilabs.com>
2019-05-29 16:08:07 +0530

    Updates

---
+++ precache-manifest.62cf4c7600bc196a17909f2661e3f9e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82d818215d44aa224e8baf511047127",
+    "url": "/speech-form/index.html"
+  },
+  {
+    "revision": "d5051877aebfd5325bc0",
+    "url": "/speech-form/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "6ad9966c5e1a979aae0e",
+    "url": "/speech-form/static/css/main.5209d037.chunk.css"
+  },
+  {
+    "revision": "d5051877aebfd5325bc0",
+    "url": "/speech-form/static/js/2.f3300468.chunk.js"
+  },
+  {
+    "revision": "6ad9966c5e1a979aae0e",
+    "url": "/speech-form/static/js/main.7bf69a5f.chunk.js"
+  },
+  {
+    "revision": "47b05147fa0052e4c5b2",
+    "url": "/speech-form/static/js/runtime~main.5bc5f149.js"
+  },
+  {
+    "revision": "364871954823eb7c0480ee99979bbe32",
+    "url": "/speech-form/static/media/voice.36487195.svg"
+  }
+]);
